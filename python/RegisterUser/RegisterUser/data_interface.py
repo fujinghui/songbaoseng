68969fd2d5c3392863177bb82b3ed11bb1c49314
fujinghui <772725447@qq.com>
2018-03-19 00:05:57 +0800

    aa

--- python/RegisterUser/RegisterUser/data_interface.py
+++ python/RegisterUser/RegisterUser/data_interface.py
@@ -1,9 +1,13 @@
 ﻿from django.http import HttpResponse;
 from django.shortcuts import render_to_response;
 from RegisterUser.database.fdatabase import *;
+
+from django.views.decorators.csrf import csrf_exempt
 import time;
 import json;
 import pymssql;
+import os;
+import md5;
 
 #将一个对象转换成Response json
 def getResponseJson(message):
@@ -26,10 +30,11 @@ def insert(sql,param):
 #获取好友列表
 def get_friends(request):
 	user_id = request.POST.get("user_id") or request.GET.get('user_id');
+	user_id = str(user_id);
 	message = {"code":200, "msg":"", "data":None};
 	if user_id is not None:
 		#查询当前用户是否的好友
-		sql_str = "select * from users";#select users.* from (select DISTINCT(user2) from user_friend_relation where user_friend_relation.user1="+user_id+") AS result LEFT JOIN users on users.id=result.user2 order by is_login desc";
+		sql_str = "select users.* from (select DISTINCT(user2) from user_friend_relation where user_friend_relation.user1="+user_id+") AS result LEFT JOIN users on users.id=result.user2 order by is_login desc";
 		ret1 = query(sql_str);
 		message['data'] = ret1;
 	else:
@@ -44,7 +49,7 @@ def get_message(request):
 	user_id = request.POST.get("user_id") or request.GET.get("user_id");
 	message = {"code":200, "msg":"", "data":None};
 	if user_id is not None:
-		sql_str = "select * from message where (send_id="+self_id+" and receive_id="+user_id+") or (send_id="+user_id+" and receive_id="+self_id+") ORDER BY date asc";
+		sql_str = "select message.*,users.name from message left join users on message.send_id=users.id  and ((message.send_id="+self_id+" and message.receive_id="+user_id+") or (message.send_id="+user_id+" and message.receive_id="+self_id+")) ORDER BY message.date asc";
 		message['data'] = query(sql_str);
 	else:
 		pass;
@@ -58,6 +63,7 @@ def get_message(request):
 #获取用户信息
 def get_user_info(request):
 	user_id = request.POST.get("user_id") or request.GET.get("user_id");
+	user_id = str(user_id);
 	message = {"code":200, "msg":"", "data":None};
 	if user_id is not None:
 		sql_str = "select * from users where id="+user_id;
@@ -81,3 +87,59 @@ def send_message(request):
 	else:
 		pass;
 	return getResponseJson(message);
+#获取所有的房间号码
+def get_groups(request):
+	id = request.session.get("id");
+	id = str(id);
+	#user_id = string(user_id);
+	#sql_str = "select group_im.* from group_im LEFT JOIN group_user on group_user.user_id="+id+" and group_user.group_id=group_im.id";
+	sql_str = "select group_im.* from group_im,group_user where group_user.group_id=group_im.id and group_user.user_id="+id;
+	message = {"code":200, "msg":"", "data":None};
+	ret = query(sql_str);
+	message['data'] = ret;
+	return getResponseJson(message);
+#获取该群下的消息
+def get_group_message(request):
+	id = request.POST.get("group_id") or request.GET.get("group_id");
+	sql_str = "SELECT group_message.id, group_message.send_id, 0 AS receive_id, group_message.message, 0 AS is_send, group_message.[date],group_message.is_image, users.name FROM group_message left join users on group_message.send_id = users.id where group_message.group_id="+id+" ORDER BY group_message.date asc";
+	ret = query(sql_str);
+	message = {"code":200, "msg":"", "data":None};
+	message['data'] = ret;
+	return getResponseJson(message);
+#获取该群的成员
+def get_group_mitglieder(request):
+	id = request.POST.get("group_id") or request.GET.get("group_id");
+	id = str(id);
+	sql_str = "select users.* from group_user left join users on group_user.user_id=users.id where group_id="+id;
+	ret = query(sql_str);
+	message = {"code":200, "msg":"", "data":None};
+	message['data'] = ret;
+	return getResponseJson(message);
+#上传图片
+@csrf_exempt
+def upload_file(request):
+	isGroup = request.POST.get("isGroup") or request.GET.get("isGroup");
+	user_id = request.POST.get("user_id") or request.GET.get("user_id");
+	friend_id = request.POST.get("friend_id") or request.GET.get("friend_id");
+	image_file = request.FILES["image_file"];
+	message = {"code":200, "msg":"", "data":None};
+	if image_file is None:
+		message['code'] = 201;
+		message['msg'] = "没有选择文件";
+	else:
+		timeChuo = time.time();
+		filename = str(image_file);
+		m1 = md5.new();
+		m1.update(filename+str(timeChuo));
+		filename = m1.hexdigest();
+		
+		path = './static/image/im_image/';
+		if not os.path.exists(path):
+			os.makedirs(path);
+		with open(path+filename, "wb+") as destination:
+			for chunk in image_file.chunks():
+				destination.write(chunk);
+			destination.close();
+		message['data'] = [filename];									#返回文件名
+	return getResponseJson(message);
+
--- python/RegisterUser/RegisterUser/urls.py
+++ python/RegisterUser/RegisterUser/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 """
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', view.hello),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', view.hello),
     #url(r'^*', filter.index),
 	#url(r'', filter.index),		#截获所有的url
 	url(r'^[^json].*$', filter.index),
@@ -35,8 +35,12 @@ urlpatterns = [
 	url(r'^json/login$', register.login),
 	url(r'^json/play$', play.play),
 	#与用户信息相关的文件的配置
-	url(r'^json/get_friends$', data_interface.get_friends),			#获取好友列表
-	url(r'^json/get_message$', data_interface.get_message),			#获取发送的消息
-	url(r'^json/get_user_info$', data_interface.get_user_info),		#获取用户信息
-	url(r'^json/send_message$', data_interface.send_message),		#发送消息
+	url(r'^json/get_friends$', data_interface.get_friends),						#获取好友列表
+	url(r'^json/get_message$', data_interface.get_message),						#获取发送的消息
+	url(r'^json/get_user_info$', data_interface.get_user_info),					#获取用户信息
+	url(r'^json/send_message$', data_interface.send_message),					#发送消息
+	url(r'^json/get_groups$', data_interface.get_groups),						#获取所有的群,
+	url(r'^json/get_group_message$', data_interface.get_group_message),			#获取群的消息
+	url(r'^json/get_group_mitglieder$', data_interface.get_group_mitglieder),	#获取群组底下的成员
+	url(r'^json/upload_file$', data_interface.upload_file),
 ]
